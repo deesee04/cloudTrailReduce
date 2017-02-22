b2bf7f5bfa074d24cfe3fe3f90947b4ceaa03e08
Dustin Camilleri <dustin.camilleri@wheniwork.com>
2017-02-22 14:31:15 -0800

    fixes, better output

--- cloudTrailReduce.py
+++ cloudTrailReduce.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+## imports.
+
 import json
-import urllib
 import boto3
 import os, sys
 import gzip
 import botocore
 from optparse import OptionParser
 
-s3resource = boto3.resource( 's3' )
+## main aws resources.
+
+s3resource = boto3.resource('s3')
 s3client = boto3.client('s3')
+
+## custom print function.
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -18,95 +23,113 @@ def eprint(*args, **kwargs):
 def oprint(*args, **kwargs):
     print(*args, file=sys.stdout, **kwargs)
 
+## download the gzipped cloudtrail archive.
+
 def get_trail_gzip(bucket, key):
     try:
-        object = s3resource.Object(bucket, key).download_file( '/tmp/in.gzip' )
+        object = s3resource.Object(bucket, key).download_file('/tmp/in.gzip')
     except:
         raise Exception('Exception ocurred retrieving CloudTrail object S3::%s/%s' % (bucket,key))
+ 
+## extract the contents from the downloaded archive.
 
-        
 def extract_trail_gzip():
     try:
-        with gzip.open( '/tmp/in.gzip', 'rb' ) as infile:
-            with open( '/tmp/out.json', 'w' ) as outfile:
+        with gzip.open('/tmp/in.gzip', 'rb') as infile:
+            with open('/tmp/out.json', 'w') as outfile:
                 for line in infile:
                     outfile.write(line)
     except:
         raise Exception('Exception ocurred when extacting /tmp/in.gzip /tmp/out.json')
+ 
+## load in the records.
 
-        
 def load_trail_records():
-    with open( '/tmp/out.json' ) as data_file:
+    with open('/tmp/out.json') as data_file:
         try:
             trail_json = json.load(data_file)
             records = trail_json['Records']
         except:
             raise Exception('Exception ocurred when parsing /tmp/out.json to JSON object')
     return records
-    
-    
-def munge_record ( record ):
+
+## process each record. 
+## output a fully formed iam policy json string.
+
+def munge_record(record):
     try:
         event_name = record['eventName'].encode('utf-8')
         event_source = record['eventSource'].encode('utf-8')
         split_source = event_source.split(".")
         service = split_source[0]
-        type = record['userIdentity']['type'].encode('utf-8')
         try:
             arn = record['userIdentity']['arn']
-            if 'root' in arn:
-                iamId = 'root'
-            else:
-                iamId = str(arn).split('/')[1]
             policy_string = {}
             policy_string = service + ":" + event_name
             return_object = {
-                'iamId': iamId, 
-                'Effect': 'Allow', 
-                'Action': [
-                    policy_string
-                ]
+                'Version': '2012-10-17',
+                'Statement': [{
+                    'Effect': 'Allow', 
+                    'Principal': { 
+                        'AWS' : [
+                            arn
+                        ]
+                    }, 
+                    'Action': [
+                        policy_string
+                    ]
+                }]
             }
             try:
                 resources = record['resources']
-                return_object['Resource'] = []
+                return_object['Statement'][0]['Resource'] = []
                 for resource in resources:
-                    return_object['Resource'].append(resource['ARN'])
+                    return_object['Statement'][0]['Resource'].append(resource['ARN'])
             except:
                 pass
             return return_object
         except:
             pass
     except:
-        raise Exception('Exception ocurred when munging record: %s' % ( record ))
+        raise Exception('Exception ocurred when munging record: %s' % (record))
 
-def parse_policy_template ( policy_template, record ):
+## parse the policy template.
+## if the template is empty, it's our first record, so just append it.
+## if the template is NOT empty, check the existing template for the IAM ID and associated resources, appending only new values.
+
+def parse_policy_template(policy_template, record):
     if (record != None):
         match = False
         if not policy_template:
             policy_template.append(record)
         else:
             for existing_record in policy_template:
-                if existing_record['iamId'] == record['iamId']:
+                if (existing_record['Statement'][0]['Principal']['AWS'][0] in record['Statement'][0]['Principal']['AWS']):
                     match = True
-                    if (record['Action'][0] in existing_record['Action']):
+                    if (record['Statement'][0]['Action'][0] in existing_record['Statement'][0]['Action']):
                         pass
                     else:
-                        existing_record['Action'].append(record['Action'][0])
+                        existing_record['Statement'][0]['Action'].append(record['Statement'][0]['Action'][0])
+                        existing_record['Statement'][0]['Action'] = sorted(existing_record['Statement'][0]['Action'])
                     try:
-                        if (record['Resource'][0] in existing_record['Resource']):
+                        if (record['Statement'][0]['Resource'][0] in existing_record['Statement'][0]['Resource']):
                             pass
                         else:
-                            existing_record['Resource'].append(record['Resource'][0])
+                            existing_record['Statement'][0]['Resource'].append(record['Statement'][0]['Resource'][0])
+                            existing_record['Statement'][0]['Resource'] = sorted(existing_record['Statement'][0]['Resource'])
                     except:
                         pass
             if not match:
                 policy_template.append(record)
     return policy_template
 
-def post_policy_template ( policy_template ):
+## print the final template.
+
+def post_policy_template(policy_template):
     #with open('/tmp/iamOut.json', 'w') as f:
     oprint(json.dumps(policy_template, indent=4))
+
+## run the application logic.
 
 def run(bucket, date, region):
     account_num = boto3.client('sts').get_caller_identity().get('Account')
@@ -123,14 +146,16 @@ def run(bucket, date, region):
         extract_trail_gzip()
         trail_records = load_trail_records()
         for record in trail_records:
-            record = munge_record ( record )
-            policy_template = parse_policy_template( policy_template, record )
+            record = munge_record (record)
+            policy_template = parse_policy_template(policy_template, record)
         i = i + 1
         completed_string = "completed [" + str(i) + "/" + str(total_objects) + "]"
         eprint(completed_string, matched_object.key)
-
-    
+ 
     post_policy_template(policy_template)
+
+## init options.
+## call the main application logic. 
 
 if __name__ == "__main__":
 
@@ -145,4 +170,3 @@ if __name__ == "__main__":
         sys.exit()
     else:
         run(options.bucket, options.date, options.region)
-
